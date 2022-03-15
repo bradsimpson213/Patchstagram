5744604883d0ca285004828d1f3207729b39b48f
Brad Simpson <bradsimpson@icloud.com>
2022-03-14 23:27:52 -0400

    initial commit, landing page setup

---
+++ src/components/Landing/index.js
@@ -0,0 +1,13 @@
+
+const Landing = () => {
+ return(
+    <div className="App">
+        <header className="App-header">
+            <h1 className="splash-header">Patchstagram</h1>
+            <img src="https://res.cloudinary.com/app-academy4/image/upload/v1647291502/Patchstagram/patch_hd_riobbp.png" className="App-logo" alt="logo" />
+            <p className="splash-detail">The cat with so much to talk about, he needs his own social media site!</p>   
+        </header>
+    </div>
+ )};
+
+ export default Landing;
