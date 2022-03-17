6d05f43c8a1f484a3363d48a2a178ca5284d0e30
Brad Simpson <bradsimpson@icloud.com>
2022-03-17 14:12:30 -0400

    Navbar pretty much done

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,24 +8,32 @@ const Navbar = () => {
 
     return(
         <div className="nav-container">
-            <div className="nav-search">
-                
-                <input className="search-input" 
-                    type="text" 
-                    value={ search } 
-                    onChange={ setSearch }
-                />
-            </div>
-            <div>
-                <NavLink to="/">
-                    <MdHomeFilled className="nav-icon"/>
-                </NavLink>
-                <NavLink to="/">
-                    <MdOutlineAddBox className="nav-icon"/>
-                </NavLink>
-                <NavLink to="/">
-                    <MdFavoriteBorder className="nav-icon"/>
-                </NavLink>
+            <div className="nav-content-container">
+                <div className="nav-logo-container">
+                    <img className="nav-logo" 
+                        src="https://res.cloudinary.com/app-academy4/image/upload/v1647291502/Patchstagram/patch_hd_riobbp.png" 
+                        alt="cat-logo" 
+                    />
+                    <h1 className="nav-title">Patchstagram</h1>
+                </div>
+                <div className="nav-search">
+                    <input className="search-input" 
+                        type="text" 
+                        value={ search } 
+                        onChange={ setSearch }
+                    />
+                </div>
+                <div className="icons-container">
+                    <NavLink to="/">
+                        <MdHomeFilled className="nav-icon"/>
+                    </NavLink>
+                    <NavLink to="/">
+                        <MdOutlineAddBox className="nav-icon"/>
+                    </NavLink>
+                    <NavLink to="/">
+                        <MdFavoriteBorder className="nav-icon"/>
+                    </NavLink>
+                </div>
             </div>
         </div>
 )};
