4980e130c66757030c78d2de785456e2e164ee96
Brad Simpson <bradsimpson@icloud.com>
2022-03-22 19:13:17 -0400

    Posts and feed is about where I want it

--- src/App.js
+++ src/App.js
@@ -4,21 +4,16 @@ import './App.css';
 import Landing from './components/Landing';
 import Navbar from './components/Navbar';
 import Feed from "./components/Feed";
+import Footer from './components/Footer';
 import { users, posts} from './data';
+
 
 const App = () => {
   const [user, setUser] = useState(users ? users[0].fullName : ''); 
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <Landing users={ users } setUser={ setUser } />
-        </Route>
-        <Route exact path="/feed">
-          <Navbar user={ user } />
-          <Feed user={ user } posts={ posts } />
-        </Route>
-      </Switch>
+      <h1>This is my Instagram Clone</h1>
+      <h2>Its sort of sad... 😢</h2>
     </div>
   );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,23 @@
-import { MdCopyright } from 'react-icons/md';
-
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { profileLinks } from '../../config';
+import './index.css';
 
 const Footer = () => {
-
+    const { linkedin, github, email } = profileLinks;
+    
     return (
-        <>
-            <span>
-                <MdCopyright />
-                Brad Simpson 2022
-            </span>           
-        </>
+        <footer className="footer">
+            <span className="text">Created by Brad Simpson ©2022</span>
+            <a href={github} >
+                <FaGithub className="icons"/>
+            </a>
+            <a href={linkedin} >
+                <FaLinkedin className="icons" />
+            </a>
+            <a href={`mailto:${email}`}>
+                <FaEnvelope className="icons" />
+            </a> 
+        </footer>
 )};
 
 export default Footer;
